fe62cc2564468e192b4c49e2ee06eeb8a5f1a4b9
unknown <samplaneis@hotmail.com>
2021-02-08 00:41:34 +0000

    Added some new features, namely that the bot responds to members leaving, and when it joins guilds

--- Bot.py
+++ Bot.py
@@ -2,13 +2,13 @@ import discord
 from discord.ext import commands
 import random
 
-#Selection of games 
-games = ['Sea of Thieves', 'Dead by Daylight', 'Team Fortress 2', 'Titanfall 2', 'Apex Legends', 'Town of Salem', 'Fall Guys', 'Papers, Please', 'Among Us', 'Prison Architect']
+#Selection of my favourite games 
+games = ['Sea of Thieves', 'Dead by Daylight', 'Team Fortress 2', 'Titanfall 2', 'Apex Legends', 'Town of Salem', 'Fall Guys', 'Papers, Please', 'Among Us', 'Prison Architect', 'Overwatch']
 
 #A client is our connection to discord
 client = discord.Client()
 
-#Prepares for use of intents, these are needed so we can welcome new members to the guild
+#Prepares for use of intents
 intents = discord.Intents.default()
 intents.members = True
 
@@ -29,7 +29,6 @@ async def on_message(message):
         return
 
     if message.content == 'Hello there':
-        print('Kenobi')
         await message.channel.send('General Kenobi')
         
     elif message.content == 'What game should I play?':
@@ -40,8 +39,23 @@ async def on_message(message):
 @client.event
 async def on_member_join(member):
     print('New member recognised')
-    channel_id = client.get_channel(784881512230813716)
-    await channel_id.send('Hello ' + member.name+'!')
+    channel_id = client.get_channel()   #The ID for the channel to send the message to should be passed in here
+    await channel_id.send('Hello ' + member.name + '!')
+
+
+#Responds to someone leaving a server
+@client.event
+async def on_member_remove(member):
+    print(member.name + ' has left')
+    channel_id = client.get_channel() #The ID for the channel to send the message to should be passed in here
+    await channel_id.send(member.name + 'has left :(')
     
-#Add the bot's token in here as a string format
+#Bot announces 
+@client.event
+async def on_guild_join(guild):
+    print('Joined a new guild')
+    channel_id = client.get_channel() #The ID for the channel to send the message to should be passed in here
+    await channel_id.send("Hey everyone, I'm CitricAmoebot! I'm just a funny, friendly personality created by CitricAmoeba, and I'm happy to be here and interact with you all!")
+
+#I've taken my bot token out for security reasons, but this will need a bot token to work
 client.run('')
--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Contains all the code for my discord bot, so I can maintain version control
 
 Don't worry everyone I've taken my bot token out as that's not meant to be posted online
 
-I made this because I haven't worked in python in a while and fancied picking it back up. I recently started using discord and thought trying to make a bot for it once I
-realised discord allowed such things and saw what they could do.
+I made this because I haven't worked in python in a while and fancied picking it back up. I recently started using discord and
+thought trying to make a bot for it would be interesting once I realised discord allowed such things and saw what they could do.
 
 The bot is designed to be fairly unintrusive, responding to events and carrying out simple tasks, automating a couple of basic bits and pieces in a discord server
 (and its also a slight memelord XD)
